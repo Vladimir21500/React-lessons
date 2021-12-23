cb160567fb452930464e1926c9f095293d5bc6f3
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-23 19:05:14 +0300

    change name classes

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -6,9 +6,9 @@ const formatDate = date => moment(date).format('DD MMM YY');
 
 const Profile = props => {
   const element = (
-    <div className="user">
-      <h1 className="user-name">{`${props.userData.firstName} ${props.userData.lastName}`}</h1>
-      <span className="user-info">{`Was  born ${formatDate(props.userData.birthDate)} in ${
+    <div className="profile">
+      <h1 className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</h1>
+      <span className="profile__birth">{`Was  born ${formatDate(props.userData.birthDate)} in ${
         props.userData.birthPlace
       }`}</span>
     </div>
