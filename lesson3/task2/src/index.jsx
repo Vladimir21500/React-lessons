aff2808a029fc4b0d4285ea04e9eedb326a07f14
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-23 17:04:30 +0300

    add Search.jsx

--- lesson3/task2/src/index.jsx
+++ lesson3/task2/src/index.jsx
@@ -1,36 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import Search from './Search.jsx';
 import './index.scss';
-import './search.scss';
 
 const rootElement = document.querySelector('#root');
 
-/* const SearchField = props => {
-  const element = (
-    <div className="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-      <div className="search__field">
-        <input type="text" className="search__input" />
-        <button className="search__button">Search</button>
-      </div>
-    </div>
-  );
-
-  return element;
-}; */
-
-class SearchField extends Component {
-  render() {
-    return (
-      <div className="search">
-        <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
-        <div className="search__field">
-          <input type="text" className="search__input" />
-          <button className="search__button">Search</button>
-        </div>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<SearchField name="Bob" />, rootElement);
+ReactDOM.render(<Search name="Bob" />, rootElement);
