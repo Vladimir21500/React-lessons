001a98c03392936e4b02c67ff7bb1564a30c5a29
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-23 16:47:09 +0300

    created

---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Comment from './Comment.jsx';
+import './styles.scss';
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com',
+};
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Comment user={userInfo} text="Good job!" date={new Date()} />, rootElement);
