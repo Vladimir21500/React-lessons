7ad13ebb02d8d6a39e92454c6d1b02b35cd0c379
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-24 06:53:08 +0300

    change tags

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -7,10 +7,10 @@ const formatDate = date => moment(date).format('DD MMM YY');
 const Profile = props => {
   const element = (
     <div className="profile">
-      <h1 className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</h1>
-      <span className="profile__birth">{`Was  born ${formatDate(props.userData.birthDate)} in ${
+      <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
+      <div className="profile__birth">{`Was  born ${formatDate(props.userData.birthDate)} in ${
         props.userData.birthPlace
-      }`}</span>
+      }`}</div>
     </div>
   );
 
