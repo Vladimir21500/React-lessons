d9af2d0b77499d932bb636ce37e2a2e7e7a96618
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2021-12-24 09:09:52 +0300

    delete space

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = props => {
   const element = (
     <div className="profile">
       <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
-      <div className="profile__birth">{`Was  born ${formatDate(props.userData.birthDate)} in ${
+      <div className="profile__birth">{`Was born ${formatDate(props.userData.birthDate)} in ${
         props.userData.birthPlace
       }`}</div>
     </div>
