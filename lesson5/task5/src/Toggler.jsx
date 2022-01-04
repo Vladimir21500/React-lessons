91fc43544233900e1fa23de08334f94f43a050bc
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-04 17:57:13 +0300

    created

---
+++ lesson5/task5/src/Toggler.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
+class Colors extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      condition: 'Off',
+    };
+  }
+  setCondition = e => {
+    this.setState({
+      condition: e.target.textContent === 'Off' ? 'On' : 'Off',
+    });
+  };
+
+  render() {
+    return (
+      <div onClick={this.setCondition} className="toggler">
+        {this.state.condition}
+      </div>
+    );
+  }
+}
+
+export default Colors;
