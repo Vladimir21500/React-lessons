e38bc0e372da094c3adb19bd0777d504b204b5f7
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-04 17:24:08 +0300

    created

---
+++ lesson5/task3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import GoodButton from './Counter.jsx';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<GoodButton />, rootElement);
