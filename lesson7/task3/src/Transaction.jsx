beb55c22ac1702fad26bd1cc7a33599aee647097
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-07 16:59:10 +0300

    change format

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -2,18 +2,11 @@ import React from 'react';
 import moment from 'moment';
 
 const transaction = ({ time, from, to, rate, amount }) => {
-  const date = moment(time).format('lll').split('');
-  const month = date
-    .splice(0, 3)
-    .reverse()
-    .reduce((symbol, acc) => acc + symbol);
   return (
     <li className="transaction">
-      <span className="transaction__date">{new Date(time).getDate() + ' ' + month}</span>
-      <span className="transaction__time">
-        {`${new Date(time).getHours()}:${new Date().getMinutes()}`}
-      </span>
-      <span className="transaction__assets"> {`${from}→${to}`} </span>
+      <span className="transaction__date">{moment(new Date(time)).format('DD MMM')}</span>
+      <span className="transaction__time">{moment(new Date(time)).format('hh:mm')}</span>
+      <span className="transaction__assets"> {`${from} → ${to}`} </span>
       <span className="transaction__rate">{rate}</span>
       <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>
     </li>
