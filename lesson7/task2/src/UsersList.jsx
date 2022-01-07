1fef7d68b1352c2b98d076cc41424f78f81e7556
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-07 11:16:27 +0300

    change name UserList

--- lesson7/task2/src/UsersList.jsx
+++ lesson7/task2/src/UsersList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import User from './User.jsx';
 
-class UserList extends Component {
+class UsersList extends Component {
   constructor(props) {
     super(props);
 
@@ -43,4 +43,4 @@ class UserList extends Component {
   }
 }
 
-export default UserList;
+export default UsersList;
--- lesson7/task2/src/index.jsx
+++ lesson7/task2/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import UserList from './UserList.jsx';
+import UsersList from './UsersList.jsx';
 
 const users = [
   {
@@ -28,4 +28,4 @@ const users = [
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<UserList users={users} />, rootElement);
+ReactDOM.render(<UsersList users={users} />, rootElement);
