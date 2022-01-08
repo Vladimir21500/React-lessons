c2d1ee08616b19245e34eb3a50cf2190639310f8
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-08 15:36:31 +0300

    delete space for valute and formated transactions bebore work

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import moment from 'moment';
 
-const transaction = ({ time, from, to, rate, amount }) => {
+const transaction = ({ amount, day, hour, from, to, rate }) => {
   return (
     <li className="transaction">
-      <span className="transaction__date">{moment(new Date(time)).format('DD MMM')}</span>
-      <span className="transaction__time">{moment(new Date(time)).format('hh:mm')}</span>
-      <span className="transaction__assets"> {`${from} → ${to}`} </span>
+      <span className="transaction__date">{day}</span>
+      <span className="transaction__time">{hour}</span>
+      <span className="transaction__assets"> {`${from}→${to}`} </span>
       <span className="transaction__rate">{rate}</span>
       <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>
     </li>
