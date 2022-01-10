7cba688833038cd40b6bead0a68059563521b8d7
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-10 13:55:31 +0300

    delete space for valutes

--- lesson7/task3/src/Transaction.jsx
+++ lesson7/task3/src/Transaction.jsx
@@ -8,7 +8,7 @@ const Transaction = ({ time, from, to, rate, amount }) => {
     <li className="transaction">
       <span className="transaction__date">{moment(new Date(time)).format('DD MMM')}</span>
       <span className="transaction__time">{moment(new Date(time)).format('hh:mm')}</span>
-      <span className="transaction__assets"> {`${from} → ${to}`} </span>
+      <span className="transaction__assets">{`${from} → ${to}`}</span>
       <span className="transaction__rate">{formatter.format(rate)}</span>
       <span className="transaction__amount">{formatter.format(amount)}</span>
     </li>
