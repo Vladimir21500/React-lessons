c46bad315e745776b3aaa8a2d2008eaab5be7fb9
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-12 13:17:15 +0300

    change componentDidUpdate

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -18,7 +18,7 @@ class Life extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-    return nextProps.number % 2;
+    return prevProps.number % 2;
   }
 
   componentWillUnmount() {
