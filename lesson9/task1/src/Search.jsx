efc07d6a6e30c1863d1eb5855ff0c5ad7af5a9d0
Vladimir21500 <vova.datsiuk.20011@gmail.com>
2022-01-13 09:10:40 +0300

    created git commit -m created

---
+++ lesson9/task1/src/Search.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  search = event => {
+    event.preventDefault();
+    alert(`Search text: ${this.state.value}`);
+  };
+
+  render() {
+    return (
+      <form className="search" onSubmit={this.search}>
+        <input
+          type="text"
+          className="search__input"
+          onChange={this.handleChange}
+          value={this.state.value}
+        />
+        <button className="search__button">Search</button>
+      </form>
+    );
+  }
+}
+
+export default Search;
